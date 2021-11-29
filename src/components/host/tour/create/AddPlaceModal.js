7e836a26ac4e80f247e693d2c2c44ea3a42a58ff
maxjhonson <max.jhonson07@gmail.com>
2021-11-29 18:20:06 -0400

    continuar

--- src/components/host/tour/create/AddPlaceModal.js
+++ src/components/host/tour/create/AddPlaceModal.js
@@ -5,6 +5,8 @@ import {
   Autocomplete,
   Marker,
 } from "@react-google-maps/api";
+import { Field, Form } from "react-final-form";
+import { InputWithValidation } from "./FieldsComponents";
 const containerStyle = {
   width: "100%",
   height: "400px",
@@ -14,13 +16,53 @@ const center = {
   lat: 18.4725357,
   lng: -69.8923817,
 };
+
+const getAdreessObj = (addressComponents) => {
+  console.log("cd", addressComponents);
+  const dirrectionObj = {
+    street: null,
+    streetNumber: null,
+    locality: null,
+    administrativeArea: null,
+  };
+  if (!addressComponents || !Array.isArray(addressComponents))
+    return { ...dirrectionObj };
+
+  addressComponents.forEach((element) => {
+    if (element.types.includes("route")) {
+      dirrectionObj.street = element.long_name;
+    } else if (element.types.includes("street_number")) {
+      dirrectionObj.streetNumber = element.long_name;
+    } else if (element.types.includes("locality")) {
+      dirrectionObj.locality = element.long_name;
+    } else if (element.types.includes("administrative_area_level_1")) {
+      dirrectionObj.administrativeArea = element.long_name;
+    }
+  });
+  return { ...dirrectionObj };
+};
+
 function AddPlaceModal(props) {
   const [autocomplete, setAutoComplete] = useState(null);
+  const [erros, setErrors] = useState({});
+  const [place, setPalce] = useState("");
+  const [street, setStreet] = useState("");
+  const [number, setNumber] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [geolocation, setGeolocation] = useState("");
 
   const onPlaceChanged = () => {
     if (autocomplete !== null) {
-      console.log(autocomplete.getPlace().geometry.location.lat());
-      console.log(autocomplete.getPlace().geometry.location.lng());
+      const autocompletePlace = autocomplete.getPlace();
+      if (!autocompletePlace) return;
+      const adreessObj = getAdreessObj(autocompletePlace?.address_components);
+      setPalce(autocompletePlace.name);
+      setStreet(adreessObj.street);
+      setNumber(adreessObj.streetNumber);
+      setCity(adreessObj.locality);
+      setState(adreessObj.administrativeArea);
+      console.log(autocomplete.getPlace()?.name);
     } else {
       console.log("Autocomplete is not loaded yet!");
     }
@@ -30,6 +72,15 @@ function AddPlaceModal(props) {
     console.log("autocomplete: ", autocomplete);
     setAutoComplete(autocomplete);
     //this.autocomplete = autocomplete
+  };
+
+  const onSubmit = () => {
+    const errorList = validate({ place, street, number, city, state });
+    if (Object.keys(errorList).length === 0) {
+    } else {
+      console.log(errorList);
+      setErrors(errorList);
+    }
   };
 
   return (
@@ -55,85 +106,151 @@ function AddPlaceModal(props) {
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
-          <div class="modal-body">
-            <div class="mb-3 row">
-              <label className="form-label">Nombre del lugar</label>
-              <div class="col-sm-10">
-                <input
-                  type="password"
-                  class="form-control"
-                  id="inputPassword"
-                />
-              </div>
-            </div>
-            <LoadScript googleMapsApiKey="" libraries={["places"]}>
-              <GoogleMap
-                mapContainerStyle={containerStyle}
-                center={center}
-                zoom={10}
-              >
-                <Marker
-                  position={{ lat: 18.4636456, lng: -69.91726280000002 }}
-                ></Marker>
-                <Marker
-                  position={{ lat: 16.4636456, lng: -69.91726280000002 }}
-                ></Marker>
-                <Autocomplete
-                  onLoad={onLoad}
-                  onPlaceChanged={onPlaceChanged}
-                  restrictions={{ country: ["do"] }}
-                >
-                  <input
-                    className="form-control"
-                    type="text"
-                    placeholder="Nombre del lugar"
-                    onChange={onPlaceChanged}
-                    style={{
-                      boxSizing: `border-box`,
-                      border: `1px solid transparent`,
-                      width: `90%`,
-                      height: `32px`,
-                      padding: `0 12px`,
-                      borderRadius: `3px`,
-                      boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                      fontSize: `14px`,
-                      outline: `none`,
-                      textOverflow: `ellipses`,
-                      position: "absolute",
-                      left: "30%",
-                      marginLeft: "-120px",
-                    }}
-                  />
-                </Autocomplete>
-              </GoogleMap>
-            </LoadScript>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-dismiss="modal"
-            >
-              Close
-            </button>
-            <button type="button" class="btn btn-primary">
-              Send message
-            </button>
-          </div>
+          <Form
+            mutators={{
+              onPlaceChanged: (args, state, utils) => {
+                console.log(args);
+                utils. (state, "street", () => "Calle");
+                console.log(state);
+              },
+            }}
+            onSubmit={onSubmit}
+          >
+            {(props) => (
+              <form onSubmit={props.handleSubmit}>
+                <div class="modal-body">
+                  <LoadScript
+                    googleMapsApiKey=""
+                    libraries={["places"]}
+                  >
+                    <Autocomplete
+                      onLoad={onLoad}
+                      onPlaceChanged={props.form.mutators.onPlaceChanged}
+                      restrictions={{ country: ["do"] }}
+                    >
+                      <div class="mb-3 row">
+                        <label className="form-label">Geolocalizacióna*</label>
+                        <div class="col-sm-12">
+                          <Field
+                            name="geolocalization"
+                            render={(props) =>
+                              InputWithValidation(
+                                props.input,
+                                props.meta,
+                                false
+                              )
+                            }
+                          />
+                        </div>
+                        <div class="invalid-feedback">{}</div>
+                      </div>
+                    </Autocomplete>
+                    <GoogleMap
+                      mapContainerStyle={containerStyle}
+                      center={center}
+                      zoom={10}
+                      onDblClick={(e) => console.log(e)}
+                    >
+                      <Marker
+                        position={{ lat: 18.4636456, lng: -69.91726280000002 }}
+                      ></Marker>
+                      <Marker
+                        position={{ lat: 16.4636456, lng: -69.91726280000002 }}
+                      ></Marker>
+                    </GoogleMap>
+                  </LoadScript>
+                  <p style={{ lineHeight: "15px" }}>
+                    <small>
+                      NOTA: Si uno de los siguientes campos no se completó de
+                      forma correcta favor corregirlos
+                    </small>
+                  </p>
+                  <div class="mt-2 row">
+                    <div className="col-md-12">
+                      <label className="form-label">
+                        Nombre del lugar/edificio*
+                      </label>
+                      <Field
+                        name="place"
+                        render={(props) =>
+                          InputWithValidation(props.input, props.meta, false)
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div class="mt-2 row">
+                    <div className="col-md-8">
+                      <label className="form-label">Calle*</label>
+                      <Field
+                        name="street"
+                        render={(props) =>
+                          InputWithValidation(props.input, props.meta, false)
+                        }
+                      />
+                    </div>
+                    <div className="col-md-4">
+                      <label className="form-label">Numero</label>
+                      <Field
+                        name="number"
+                        render={(props) =>
+                          InputWithValidation(props.input, props.meta, false)
+                        }
+                      />
+                    </div>
+                  </div>
+                  <div class="mt-2 row">
+                    <div className="col-md-6">
+                      <label className="form-label">Ciudad*</label>
+                      <Field
+                        name="city"
+                        render={(props) =>
+                          InputWithValidation(props.input, props.meta, false)
+                        }
+                      />
+                    </div>
+                    <div className="col-md-6">
+                      <label className="form-label">Provincia*</label>
+                      <Field
+                        name="state"
+                        render={(props) =>
+                          InputWithValidation(props.input, props.meta, false)
+                        }
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button
+                    type="button"
+                    class="btn btn-secondary"
+                    data-dismiss="modal"
+                  >
+                    Cerrar
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    onClick={props.form.mutators.onPlaceChanged}
+                  >
+                    Guardar
+                  </button>
+                </div>
+              </form>
+            )}
+          </Form>
         </div>
       </div>
     </div>
   );
 }
-const mapStyle = {
-  width: "100%",
-  height: "500px",
-};
 
-const mapContainerStyle = {
-  position: "initial",
-  width: "50%",
-  height: "50%",
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.place) errors.place = "Este campo es requerido";
+  if (!formValues.street) errors.street = "Este campo es requerido";
+  if (!formValues.city) errors.city = "Este campo es requerido";
+  if (!formValues.state) errors.state = "Este campo es requerido";
+  return errors;
 };
 
 export default AddPlaceModal;
